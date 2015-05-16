194c805ba10dbf19eb3252e826a493e7d186cbdf
zsx <zsx@zsxsoft.com>
2015-05-16 13:14:19 +0800

    加入控制面板，同时加入FPS检测

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-(function() {
+/* global panelWindow */
+/* global global */
+(function () {
 	var gui = require('nw.gui');
-	var win = gui.Window.get();
-	win.showDevTools();
 	global.coordinator = new (require('events').EventEmitter);
-
+	global.panelWindow = gui.Window.open('panel.html', {
+		toolbar: false,
+		resizable: true,
+		width: 400,
+		height: 200
+	});
 	var config = require("./config");
 	var danmu = require("./lib/danmu");
 	var listener = require("./lib/listener");
 	var penetrate = require("./lib/penetrate");
 
 	var crypto = require('crypto');
-	var md5 = crypto.createHash("md5");
-
-
-
-
-	document.title = "DANMU Client - Client ID = " + crypto.createHash('md5').update(Math.random().toString()).digest('hex');
 
 	var isStart = false;
-	window.addEventListener("keydown", function(e) {
+	var resizeFunction = function (e) {
+		if (!isStart) {
+			document.querySelector(".border").style.width = window.outerWidth - 6 + "px";
+			document.querySelector(".border").style.height = window.outerHeight - 6 + "px";
+		}
+	};
+
+	document.title = "DANMU Client - Client ID = " + crypto.createHash('md5').update(Math.random().toString()).digest('hex');
+	window.addEventListener("keydown", function (e) {
 		if (e.keyCode == 13 && !isStart) {
 			document.getElementById("message").remove();
 			document.querySelector(".border").remove();
@@ -29,16 +36,11 @@
 			isStart = true;
 		}
 	}, true);
-
-	var resizeFunction = function(e) {
-		if (!isStart) {
-			document.querySelector(".border").style.width = window.outerWidth - 6 + "px";
-			document.querySelector(".border").style.height = window.outerHeight - 6 + "px";
-		}
-	};
 	window.addEventListener("resize", resizeFunction);
+
 	resizeFunction();
 
-
-
+	global.panelWindow.on("windowControl", function (data) {
+	});
+	
 })();
---
+++ panel.js
@@ -0,0 +1,37 @@
+/// <reference path="typings/node/node.d.ts"/>
+(function () {
+	var gui = require('nw.gui');
+	var win = gui.Window.get();
+
+	var countQuitValue = 0;
+	document.querySelector("#btn-quit").addEventListener("click", function () {
+		if (countQuitValue == 1) {
+			process.exit();
+		} else {
+			setTimeout(function () {
+				document.querySelector("#btn-quit").innerText = "退出程序";
+				countQuitValue = 0;
+			}, 5000);
+			this.innerText = "5秒内再按一次退出";
+			countQuitValue = 1;
+		}
+		return false;
+	});
+
+	var controlButtons = window.document.querySelectorAll(".btn-control");
+	var contronClick = function () {
+		win.emit(this.getAttribute("data-top"), this.getAttribute("data-param"));
+	};
+	for (var i = 0; i < controlButtons.length; i++) {
+		controlButtons.item(i).addEventListener("click", contronClick);
+	}
+	
+	win.showDevTools('', true);
+	win.on("devtools-opened", function (url) {
+		win.closeDevTools(); // https://github.com/nwjs/nw.js/issues/2976
+		document.getElementById("iframe-href").innerText = url;
+	});
+	win.on("fps", function(fps) {
+		document.getElementById("txt-fps").innerText = fps;
+	});
+})();
