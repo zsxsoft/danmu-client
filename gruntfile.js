cc26d43c883876b56cdc60874a0376077a62c790
zsx <zsx@zsxsoft.com>
2015-05-16 10:59:56 +0800

    调整Release

--- gruntfile.js
+++ gruntfile.js
@@ -1,43 +1,72 @@
 'use strict';
 
 module.exports = function(grunt) {
+	var path = require('path');
+
 	var packageJson = grunt.file.readJSON('package.json');
-	var encode = grunt.option('encode') || 'utf8';
-	var language = grunt.option('language') || 'node';
-	var doNotCompile = grunt.option("without-compile") || false;
-	language = language.toLowerCase();
+	var platforms = ['win32', 'win64'];
+	var buildDir = './builds/';
+	var extDir = './cache/node_modules/';
+	var exeName = 'nw';
 
 	grunt.initConfig({
 		pkg: packageJson,
 		nodewebkit: {
 			options: {
-				platforms: ['win'],
-				buildDir: './webkitbuilds',
-				appName: "nw"
+				version: "0.12.1", // https://github.com/mllrsohn/node-webkit-builder/issues/222
+				platforms: platforms,
+				buildDir: buildDir,
+				appName: exeName,
+				winIco: "./icon.ico"
 			},
-			src: ['./*', './lib/**', './node_modules/socket.io-client/**', './node_modules/nw-penetrate/**'], // Your node-webkit app
-			
+			src: ['package.json'], // Your node-webkit app
 		},
 		jshint: {
 			options: {
-				"-W061": true
+				"-W061": true,
+				"multistr": true
 			},
 			src: ['./app.js', './lib/*'],
+		},
+		copy: {
+			main: {
+				files: (function() {
+					var copyFiles = [];
+					platforms.forEach(function(platform) {
+						copyFiles.push({
+							expand: true,
+							src: [
+								'./package.json', './index.js', './index.html', './config.js', './lib/**', 
+								'./node_modules/socket.io-client/**', 
+								'./node_modules/nw-penetrate/package.json', './node_modules/nw-penetrate/bindings-fork/**', './node_modules/nw-penetrate/index.js'],
+							dest: buildDir + exeName + "/" + platform
+						});
+						copyFiles.push({
+							cwd: extDir + platform,
+							src: ['./**'],
+							dest: buildDir + exeName + "/" + platform + '/node_modules',
+							expand: true
+						});
+					});
+					return copyFiles;
+				})()
+			}
 		},
 	});
 
 
 	grunt.loadNpmTasks('grunt-node-webkit-builder');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 
 	grunt.registerTask('default', 'danmu-client', function() {
 		var tasks = [];
-		tasks.push("jshint", "nodewebkit");
+		tasks.push("jshint");
+		tasks.push("nodewebkit");
+		tasks.push("copy");
 		grunt.task.run(tasks);
 
-		// 懒得用grunt插件了
-		
 	});
 
 
