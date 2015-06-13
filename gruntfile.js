ece7277c0586465972a1f305a8c8ffb90b7f95e2
zsx <zsx@zsxsoft.com>
2015-06-13 16:59:42 +0800

    将rid加入自动化

--- gruntfile.js
+++ gruntfile.js
@@ -8,6 +8,7 @@ module.exports = function(grunt) {
 	var buildDir = './builds/';
 	var extDir = './cache/node_modules/';
 	var exeName = 'nw';
+	var realExeName = 'danmu';
 
 	grunt.initConfig({
 		pkg: packageJson,
@@ -31,6 +32,10 @@ module.exports = function(grunt) {
 		copy: {
 			main: {
 				files: (function() {
+					/**
+					 * grunt-node-webkit不能针对特殊平台单独拷贝文件
+					 * 所以这里只能选用copy来代替
+					 */
 					var copyFiles = [];
 					platforms.forEach(function(platform) {
 						copyFiles.push({
@@ -52,20 +57,32 @@ module.exports = function(grunt) {
 				})()
 			}
 		},
+		commands: {
+			makerid: {
+				cmd: (function() {
+					return 'cd builds && (for /r %i in (*.node) do @echo %i && rid %i nw.exe ' + realExeName + '.exe) && (for /d %i in (nw\\win*) do @ren %i\\nw.exe ' + realExeName + '.exe)';
+				})(),
+				//force: true
+			}
+		}
 	});
 
 
 	grunt.loadNpmTasks('grunt-node-webkit-builder');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-commands');
 
 
-	grunt.registerTask('default', 'danmu-client', function() {
-		var tasks = [];
-		tasks.push("jshint");
-		tasks.push("nodewebkit");
-		tasks.push("copy");
-		grunt.task.run(tasks);
+	grunt.registerTask('default', 'danmu-client', function() {	
+		var os = require('os');
+		var platform = os.platform();
+		var taskList = ['jshint', 'nodewebkit', 'copy'];
+		if (platform == "win32" || platform == "win64") {
+			taskList.push('commands:makerid');
+		}
+		grunt.task.run(taskList);
+
 
 	});
 
