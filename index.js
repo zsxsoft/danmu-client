7f4d517782f543e8b3110d41f380e7416abbe119
zsx <zsx@zsxsoft.com>
2015-09-01 19:11:57 +0800

    补充必要提示

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 /* global panelWindow */
 /* global global */
-(function () {
-	var gui = require('nw.gui');
-	global.coordinator = new (require('events').EventEmitter);
-	global.panelWindow = gui.Window.open('panel.html', {
-		toolbar: false,
-		resizable: true,
-		width: 400,
-		height: 200
-	});
+(function() {
+    var gui = require('nw.gui');
+    global.coordinator = new(require('events').EventEmitter);
+    global.panelWindow = gui.Window.open('panel.html', {
+        toolbar: false,
+        resizable: true,
+        width: 400,
+        height: 200
+    });
 
 
-	var config = require("./config");
-	global.config = config;
-	var danmu = require("./lib/danmu");
-	var listener = require("./lib/listener");
-	var penetrate = require("./lib/penetrate");
-	var crypto = require('crypto');
-	var isStart = false;
-	var keydownFunction = function (e) {
+    var config = require("./config");
+    var danmu = require("./lib/danmu");
+    var listener = require("./lib/listener");
+    var penetrate = require("./lib/penetrate");
+    var crypto = require('crypto');
+    var packageJson = require("./package.json");
 
-		if (e.keyCode == 13 && !isStart) {
-			document.getElementById("message").remove();
-			document.querySelector(".border").remove();
-			document.querySelector("body").style.background = "transparent";
+    var isStart = false;
+    global.config = config;
 
-			listener.init(config);
-			danmu.init(config, listener, document.getElementById("main-canvas"));
-			penetrate.init();
+    var keydownFunction = function(e) {
+        switch (e.keyCode) {
+            case 13:
+                if (!isStart) {
+                    document.getElementById("message").remove();
+                    document.querySelector(".border").remove();
+                    document.querySelector("body").style.background = "transparent";
 
-			isStart = true;
-		}
+                    listener.init(config);
+                    danmu.init(config, listener, document.getElementById("main-canvas"));
+                    penetrate.init();
 
-		if (e.keyCode == 123) { // F12
-			gui.Window.get().showDevTools();
-		}
+                    isStart = true;
+                }
+                break;
+            case 112:
+            	gui.Shell.openExternal(packageJson.homepage);
+                break;
+            case 123:
+                gui.Window.get().showDevTools();
+                break;
+        }
+    }
 
-	}
+    document.getElementById("client-version").innerHTML = packageJson.version;
+    document.getElementById("client-homepage").innerHTML = packageJson.homepage;
+    document.getElementById("client-author").innerHTML = packageJson.author;
 
-	document.title = "DANMU Client - Client ID = " + crypto.createHash('md5').update(Math.random().toString()).digest('hex');
-	window.addEventListener("keydown", keydownFunction, true);
+    document.title = "DANMU Client - Client ID = " + crypto.createHash('md5').update(Math.random().toString()).digest('hex');
+    window.addEventListener("keydown", keydownFunction, true);
 
 })();
