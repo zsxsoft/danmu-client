734c6c981d9bd23c1790e933ee0bd14fc8b8d181
zsx <zsx@zsxsoft.com>
2015-10-01 20:16:54 +0800

    细节调整

--- main.js
+++ main.js
@@ -23,21 +23,25 @@ app.on('ready', function () {
         resizable: true,
         title: "DANMU Client", 
         "always-on-top": true,
+        icon: __dirname + '/danmu.png'
     });
     windows.mainWindow.loadUrl('file://' + __dirname + '/index.html');
     windows.mainWindow.on('closed', function () {
         windows.mainWindow = null;
         app.quit();
     });
+    windows.mainWindow.setMenu(null);
 
     windows.panelWindow = new BrowserWindow({
         width: 400,
         height: 200, 
-        toolbar: false
+        toolbar: false, 
+        icon: __dirname + '/danmu.png'
     });
     windows.panelWindow.loadUrl('file://' + __dirname + '/panel.html');
     windows.panelWindow.on('closed', function () {
         windows.panelWindow = null;
     });
+    windows.panelWindow.setMenu(null);
 
 });
--- panel.js
+++ panel.js
@@ -3,23 +3,31 @@
 
 	var windows = require('remote').getGlobal('windows');
 	var coordinator = require('remote').getGlobal('coordinator');
-
-
 	var controlButtons = window.document.querySelectorAll(".btn-control");
 	var countQuitValue = 0;
+	var isShow = true;
 	var controlButtonClick = function () {
 		coordinator.emit(this.getAttribute("data-top"), this.getAttribute("data-param"));
 	};
 
 	coordinator.on("fps", function (fps) {
+		if (!isShow) return;
 		document.getElementById("txt-fps").innerText = fps;
 	});
 
 	window.addEventListener("keydown", function (e) {
 		if (e.keyCode == 123) { // F12
-			win.showDevTools();
+			windows.panelWindow.openDevTools({
+				detach: true
+			});
 		}
 	}, true);
+	window.addEventListener('beforeunload', function (e) {
+		e.returnValue = 'false';
+		windows.panelWindow.hide();
+		isShow = false;
+	});
+
 	document.querySelector("#btn-quit").addEventListener("click", function () {
 		if (countQuitValue == 1) {
 			coordinator.emit("exit");
